a8b10ee7e92e76fdccddef1af27fb4d1aa58ad57
Vidita Singh <vidita1812@gmail.com>
2025-07-26 02:29:23 +0530

    update mock data

--- src/lib/mockData.ts
+++ src/lib/mockData.ts
@@ -3,8 +3,8 @@ export const mockIncidents = [
   {
     id: '1',
     type: 'Unauthorised Access',
-    tsStart: new Date(Date.now() - 3 * 60 * 60 * 1000), // 3 hours ago
-    tsEnd: new Date(Date.now() - 3 * 60 * 60 * 1000 + 70 * 60 * 1000),
+    tsStart: new Date(Date.now() - 30 * 60 * 1000), // 30 minutes ago
+    tsEnd: new Date(Date.now() - 30 * 60 * 1000 + 70 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-1.png',
     resolved: false,
     camera: {
@@ -18,8 +18,8 @@ export const mockIncidents = [
   {
     id: '2',
     type: 'Unauthorised Access',
-    tsStart: new Date(Date.now() - 1 * 60 * 60 * 1000), // 1 hour ago
-    tsEnd: new Date(Date.now() - 1 * 60 * 60 * 1000 + 90 * 60 * 1000),
+    tsStart: new Date(Date.now() - 60 * 60 * 1000), // 1 hour ago
+    tsEnd: new Date(Date.now() - 60 * 60 * 1000 + 90 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-2.png',
     resolved: false,
     camera: {
@@ -33,8 +33,8 @@ export const mockIncidents = [
   {
     id: '3',
     type: 'Gun Threat',
-    tsStart: new Date(Date.now() - 30 * 60 * 1000), // 30 minutes ago
-    tsEnd: new Date(Date.now() - 30 * 60 * 1000 + 70 * 60 * 1000),
+    tsStart: new Date(Date.now() - 90 * 60 * 1000), // 90 minutes ago
+    tsEnd: new Date(Date.now() - 90 * 60 * 1000 + 70 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-3.png',
     resolved: false,
     camera: {
@@ -48,8 +48,8 @@ export const mockIncidents = [
   {
     id: '4',
     type: 'Face Recognised',
-    tsStart: new Date(Date.now() - 45 * 60 * 1000), // 45 minutes ago
-    tsEnd: new Date(Date.now() - 45 * 60 * 1000 + 70 * 60 * 1000),
+    tsStart: new Date(Date.now() - 120 * 60 * 1000), // 2 hours ago
+    tsEnd: new Date(Date.now() - 120 * 60 * 1000 + 70 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-4.png',
     resolved: false,
     camera: {
@@ -63,8 +63,8 @@ export const mockIncidents = [
   {
     id: '5',
     type: 'Traffic Congestion',
-    tsStart: new Date(Date.now() - 2.5 * 60 * 60 * 1000), // 2.5 hours ago
-    tsEnd: new Date(Date.now() - 2.5 * 60 * 60 * 1000 + 120 * 60 * 1000),
+    tsStart: new Date(Date.now() - 150 * 60 * 1000), // 2.5 hours ago
+    tsEnd: new Date(Date.now() - 150 * 60 * 1000 + 120 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-5.png',
     resolved: false,
     camera: {
@@ -78,8 +78,8 @@ export const mockIncidents = [
   {
     id: '6',
     type: 'Traffic Congestion',
-    tsStart: new Date(Date.now() - 15 * 60 * 1000), // 15 minutes ago
-    tsEnd: new Date(Date.now() - 15 * 60 * 1000 + 85 * 60 * 1000),
+    tsStart: new Date(Date.now() - 180 * 60 * 1000), // 3 hours ago
+    tsEnd: new Date(Date.now() - 180 * 60 * 1000 + 85 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-6.png',
     resolved: false,
     camera: {
@@ -93,8 +93,8 @@ export const mockIncidents = [
   {
     id: '13',
     type: 'Face Recognised',
-    tsStart: new Date(Date.now() - 12 * 60 * 60 * 1000), // 12 hours ago
-    tsEnd: new Date(Date.now() - 12 * 60 * 60 * 1000 + 70 * 60 * 1000),
+    tsStart: new Date(Date.now() - 210 * 60 * 1000), // 3.5 hours ago
+    tsEnd: new Date(Date.now() - 210 * 60 * 1000 + 70 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-1.png',
     resolved: true,
     camera: {
@@ -108,8 +108,8 @@ export const mockIncidents = [
   {
     id: '14',
     type: 'Gun Threat',
-    tsStart: new Date(Date.now() - 10 * 60 * 60 * 1000), // 10 hours ago
-    tsEnd: new Date(Date.now() - 10 * 60 * 60 * 1000 + 120 * 60 * 1000),
+    tsStart: new Date(Date.now() - 240 * 60 * 1000), // 4 hours ago
+    tsEnd: new Date(Date.now() - 240 * 60 * 1000 + 120 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-2.png',
     resolved: true,
     camera: {
@@ -123,8 +123,8 @@ export const mockIncidents = [
   {
     id: '15',
     type: 'Gun Threat',
-    tsStart: new Date(Date.now() - 22 * 60 * 60 * 1000), // 22 hours ago
-    tsEnd: new Date(Date.now() - 22 * 60 * 60 * 1000 + 70 * 60 * 1000),
+    tsStart: new Date(Date.now() - 270 * 60 * 1000), // 4.5 hours ago
+    tsEnd: new Date(Date.now() - 270 * 60 * 1000 + 70 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-3.png',
     resolved: true,
     camera: {
@@ -138,9 +138,9 @@ export const mockIncidents = [
   {
     id: '20',
     type: 'Unauthorised Access',
-    tsStart: new Date(Date.now() - 35 * 60 * 1000), // 35 minutes ago
-    tsEnd: new Date(Date.now() - 35 * 60 * 60 * 1000 + 80 * 60 * 1000),
-    thumbnailUrl: '/images/thumbnails/incident-6.jpg',
+    tsStart: new Date(Date.now() - 300 * 60 * 1000), // 5 hours ago
+    tsEnd: new Date(Date.now() - 300 * 60 * 1000 + 80 * 60 * 1000),
+    thumbnailUrl: '/images/thumbnails/incident-6.png',
     resolved: false,
     camera: {
       id: '3',
@@ -153,8 +153,8 @@ export const mockIncidents = [
   {
     id: '21',
     type: 'Face Recognised',
-    tsStart: new Date(Date.now() - 2 * 60 * 60 * 1000), // 2 hours ago
-    tsEnd: new Date(Date.now() - 2 * 60 * 60 * 1000 + 30 * 60 * 1000),
+    tsStart: new Date(Date.now() - 330 * 60 * 1000), // 5.5 hours ago
+    tsEnd: new Date(Date.now() - 330 * 60 * 1000 + 30 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-10.jpg',
     resolved: true,
     camera: {
@@ -168,8 +168,8 @@ export const mockIncidents = [
   {
     id: '22',
     type: 'Gun Threat',
-    tsStart: new Date(Date.now() - 11 * 60 * 60 * 1000), // 11 hours ago
-    tsEnd: new Date(Date.now() - 11 * 60 * 60 * 1000 + 15 * 60 * 1000),
+    tsStart: new Date(Date.now() - 360 * 60 * 1000), // 6 hours ago
+    tsEnd: new Date(Date.now() - 360 * 60 * 1000 + 15 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-12.jpg',
     resolved: true,
     camera: {
@@ -183,8 +183,8 @@ export const mockIncidents = [
   {
     id: '23',
     type: 'Unauthorised Access',
-    tsStart: new Date(Date.now() - 16 * 60 * 60 * 1000), // 16 hours ago
-    tsEnd: new Date(Date.now() - 16 * 60 * 60 * 1000 + 90 * 60 * 1000),
+    tsStart: new Date(Date.now() - 390 * 60 * 1000), // 6.5 hours ago
+    tsEnd: new Date(Date.now() - 390 * 60 * 1000 + 90 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-11.jpg',
     resolved: true,
     camera: {
@@ -198,9 +198,9 @@ export const mockIncidents = [
   {
     id: '24',
     type: 'Traffic Congestion',
-    tsStart: new Date(Date.now() - 20 * 60 * 1000), // 20 minutes ago
-    tsEnd: new Date(Date.now() - 20 * 60 * 1000 + 120 * 60 * 1000),
-    thumbnailUrl: '/images/thumbnails/incident-5.jpg',
+    tsStart: new Date(Date.now() - 420 * 60 * 1000), // 7 hours ago
+    tsEnd: new Date(Date.now() - 420 * 60 * 1000 + 120 * 60 * 1000),
+    thumbnailUrl: '/images/thumbnails/incident-5.png',
     resolved: false,
     camera: {
       id: '1',
@@ -213,8 +213,8 @@ export const mockIncidents = [
   {
     id: '25',
     type: 'Face Recognised',
-    tsStart: new Date(Date.now() - 10 * 60 * 1000), // 10 minutes ago
-    tsEnd: new Date(Date.now() - 10 * 60 * 1000 + 90 * 60 * 1000),
+    tsStart: new Date(Date.now() - 450 * 60 * 1000), // 7.5 hours ago
+    tsEnd: new Date(Date.now() - 450 * 60 * 1000 + 90 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-1.png',
     resolved: false,
     camera: {
@@ -228,8 +228,8 @@ export const mockIncidents = [
   {
     id: '26',
     type: 'Traffic Congestion',
-    tsStart: new Date(Date.now() - 5 * 60 * 1000), // 5 minutes ago
-    tsEnd: new Date(Date.now() - 5 * 60 * 1000 + 40 * 60 * 1000),
+    tsStart: new Date(Date.now() - 480 * 60 * 1000), // 8 hours ago
+    tsEnd: new Date(Date.now() - 480 * 60 * 1000 + 40 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-2.png',
     resolved: false,
     camera: {
@@ -243,8 +243,8 @@ export const mockIncidents = [
   {
     id: '27',
     type: 'Unauthorised Access',
-    tsStart: new Date(Date.now() - 40 * 60 * 1000), // 40 minutes ago
-    tsEnd: new Date(Date.now() - 40 * 60 * 1000 + 25 * 60 * 1000),
+    tsStart: new Date(Date.now() - 510 * 60 * 1000), // 8.5 hours ago
+    tsEnd: new Date(Date.now() - 510 * 60 * 1000 + 25 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-3.png',
     resolved: false,
     camera: {
@@ -258,8 +258,8 @@ export const mockIncidents = [
   {
     id: '28',
     type: 'Face Recognised',
-    tsStart: new Date(Date.now() - 75 * 60 * 1000), // 75 minutes ago
-    tsEnd: new Date(Date.now() - 75 * 60 * 1000 + 60 * 60 * 1000),
+    tsStart: new Date(Date.now() - 540 * 60 * 1000), // 9 hours ago
+    tsEnd: new Date(Date.now() - 540 * 60 * 1000 + 60 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-4.png',
     resolved: false,
     camera: {
@@ -273,8 +273,8 @@ export const mockIncidents = [
   {
     id: '29',
     type: 'Gun Threat',
-    tsStart: new Date(Date.now() - 18 * 60 * 60 * 1000), // 18 hours ago
-    tsEnd: new Date(Date.now() - 18 * 60 * 60 * 1000 + 240 * 60 * 1000),
+    tsStart: new Date(Date.now() - 570 * 60 * 1000), // 9.5 hours ago
+    tsEnd: new Date(Date.now() - 570 * 60 * 1000 + 240 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-5.png',
     resolved: true,
     camera: {
@@ -288,8 +288,8 @@ export const mockIncidents = [
   {
     id: '30',
     type: 'Traffic Congestion',
-    tsStart: new Date(Date.now() - 25 * 60 * 1000), // 25 minutes ago
-    tsEnd: new Date(Date.now() - 25 * 60 * 1000 + 55 * 60 * 1000),
+    tsStart: new Date(Date.now() - 600 * 60 * 1000), // 10 hours ago
+    tsEnd: new Date(Date.now() - 600 * 60 * 1000 + 55 * 60 * 1000),
     thumbnailUrl: '/images/thumbnails/incident-6.png',
     resolved: false,
     camera: {
